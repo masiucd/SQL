af8e499a8a9aaa93397983d3667447be17dc0d3a
Masiu <ciszekmarcell@gmail.com>
2019-10-06 19:53:33 +0200

    delete

---
+++ partTwo/delete.sql
@@ -0,0 +1,19 @@
+
+DELETE FROM cats WHERE name='Egg';
+
+SELECT *
+FROM cats;
+
+SELECT *
+FROM cats
+WHERE name='egg';
+
+DELETE FROM cats WHERE name='egg';
+
+SELECT *
+FROM cats;
+
+DELETE FROM cats;
+
+delete from cats where age = 4;
+delete from cats where age = cat_id;
