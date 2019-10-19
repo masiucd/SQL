cb6083954bac554986c3f267347b11996d57958c
Masiu <ciszekmarcell@gmail.com>
2019-10-19 19:32:23 +0200

    string s

--- book-db/mix.sql
+++ book-db/mix.sql
@@ -1,8 +1,28 @@
-SELECT REPLACE ( concat ('i', ' ', 'like',' ','dogs'),' ','_');
+SELECT
+  author_lname AS forwards,
+  REVERSE(author_lname) AS backwards
+FROM books;
 
 
-SELECT CONCAT("The ", title )
-from books;
+SELECT
+  UPPER
+   (
+      CONCAT(author_fname, ' ', author_lname)
+   ) AS 'full name in caps'
+FROM books;
 
-select Concat(substring(title, 1 ,10),"...") as "short title", Concat(author_fname,',',author_lname) as "author", Concat(stock_quantity, " in stock") as "quantity"
-from books;
+
+SELECT
+  CONCAT(title, ' was released in ', released_year) AS blurb
+FROM books;
+SELECT
+  title,
+  CHAR_LENGTH(title) AS 'character count'
+FROM books;
+
+
+SELECT
+  CONCAT(SUBSTRING(title, 1, 10), '...') AS 'short title',
+  CONCAT(author_lname, ',', author_fname) AS author,
+  CONCAT(stock_quantity, ' in stock') AS quantity
+FROM books;
