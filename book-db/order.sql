7779b1ea3077b7a63d1236a307ea83d01e12f58a
Masiu <ciszekmarcell@gmail.com>
2019-10-20 12:58:37 +0200

    order by

---
+++ book-db/order.sql
@@ -0,0 +1,17 @@
+select title
+from books
+order by title DESC;
+
+
+select title
+from books
+order by title ASC;
+
+select released_year
+from books
+order by released_year DESC;
+
+
+select released_year
+from books
+order by released_year ASC;
