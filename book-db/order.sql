5ae7726e7e25c37a9c4698f98b227ea607e1223a
Masiu <ciszekmarcell@gmail.com>
2019-10-20 13:02:51 +0200

    order by

--- book-db/order.sql
+++ book-db/order.sql
@@ -1,17 +1,63 @@
-select title
-from books
-order by title DESC;
+ELECT author_lname
+FROM books;
 
+SELECT author_lname
+FROM books
+ORDER BY author_lname;
 
-select title
-from books
-order by title ASC;
+SELECT title
+FROM books;
 
-select released_year
-from books
-order by released_year DESC;
+SELECT title
+FROM books
+ORDER BY title;
+SELECT author_lname
+FROM books
+ORDER BY author_lname DESC;
 
+SELECT released_year
+FROM books;
 
-select released_year
-from books
-order by released_year ASC;
+SELECT released_year
+FROM books
+ORDER BY released_year;
+
+SELECT released_year
+FROM books
+ORDER BY released_year DESC;
+
+SELECT released_year
+FROM books
+ORDER BY released_year ASC;
+
+SELECT title, released_year, pages
+FROM books
+ORDER BY released_year;
+
+SELECT title, pages
+FROM books
+ORDER BY released_year;
+
+SELECT title, author_fname, author_lname
+FROM books
+ORDER BY 2;
+
+SELECT title, author_fname, author_lname
+FROM books
+ORDER BY 3;
+
+SELECT title, author_fname, author_lname
+FROM books
+ORDER BY 1;
+
+SELECT title, author_fname, author_lname
+FROM books
+ORDER BY 1 DESC;
+
+SELECT author_lname, title
+FROM books
+ORDER BY 2;
+
+SELECT author_fname, author_lname
+FROM books
+ORDER BY author_lname, author_fname;
