84503f1d85de63baf54159cc699f46057dbcda4d
Masiu <ciszekmarcell@gmail.com>
2019-11-10 12:22:31 +0100

    joins

--- ordersDB/join.sql
+++ ordersDB/join.sql
@@ -9,3 +9,39 @@ where customers.id = orders.customer_id;
 
 SELECT *
 FROM customers JOIN orders ON customers.id = orders.customer_id;
+
+
+SELECT *
+FROM customers
+  JOIN orders
+  ON customers.id = orders.customer_id;
+
+SELECT first_name, last_name, order_date, amount
+FROM customers
+  JOIN orders
+  ON customers.id = orders.customer_id;
+
+SELECT *
+FROM orders
+  JOIN customers
+  ON customers.id = orders.customer_id;
+
+-- left joins
+
+SELECT *
+FROM customers
+  LEFT JOIN orders
+  ON customers.id = orders.customer_id;
+SELECT first_name, last_name, order_date, amount
+FROM customers
+  LEFT JOIN orders
+  ON customers.id = orders.customer_id;
+SELECT
+  first_name,
+  last_name,
+  IFNULL(SUM(amount), 0) AS total_spent
+FROM customers
+  LEFT JOIN orders
+  ON customers.id = orders.customer_id
+GROUP BY customers.id
+ORDER BY total_spent;
