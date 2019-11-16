f7fdbb8e4db0cc1d34890f44d5873a9035d8ad93
Masiu <ciszekmarcell@gmail.com>
2019-11-16 12:11:17 +0100

    add

--- manyToMany/reviews.sql
+++ manyToMany/reviews.sql
@@ -138,3 +138,27 @@ CREATE TABLE series
       ON series.id = reviews.series_id
     GROUP BY series.id
     ORDER BY avg_rating;
+
+
+    SELECT
+      first_name,
+      last_name,
+      rating
+    FROM reviewers
+      INNER JOIN reviews
+      ON reviewers.id = reviews.reviewer_id;
+
+
+    SELECT
+      first_name,
+      last_name,
+      rating
+    FROM reviews
+      INNER JOIN reviewers
+      ON reviewers.id = reviews.reviewer_id;
+
+    select title as unwatched_serie
+    from series
+      left join reviews
+      on series.id = reviews.series_id
+    where rating is null;
