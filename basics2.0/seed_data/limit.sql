678e08833018bb3925c49b3a38c774ba49ea8766
Masiu <ciszekmarcell@gmail.com>
2020-02-27 13:06:29 +0100

    add

---
+++ basics2.0/seed_data/limit.sql
@@ -0,0 +1,68 @@
+select title
+from books
+where author_lname = "Eggers"
+limit 2;
+
+select author_fname, title
+from books
+ORDER by title
+limit 5;
+
+
+SELECT title
+FROM books LIMIT 3;
+
+SELECT title
+FROM books LIMIT
+1;
+
+SELECT title
+FROM books LIMIT
+10;
+
+SELECT *
+FROM books LIMIT
+1;
+
+SELECT title, released_year
+FROM books
+ORDER BY released_year DESC LIMIT 5;
+
+SELECT title
+, released_year FROM books
+ORDER BY released_year DESC LIMIT 1;
+
+SELECT title, released_year
+FROM books
+ORDER BY released_year DESC LIMIT 14;
+
+SELECT title
+, released_year FROM books
+ORDER BY released_year DESC LIMIT 0,5;
+
+SELECT title, released_year
+FROM books
+ORDER BY released_year DESC LIMIT 0,3;
+
+SELECT title
+, released_year FROM books
+ORDER BY released_year DESC LIMIT 1,3;
+
+SELECT title, released_year
+FROM books
+ORDER BY released_year DESC LIMIT 10,1;
+
+SELECT * FROM tbl
+LIMIT 95,18446744073709551615;
+
+SELECT title
+FROM books LIMIT
+5;
+
+SELECT title
+FROM books LIMIT
+5, 123219476457;
+
+SELECT title
+FROM books LIMIT
+5, 50;
