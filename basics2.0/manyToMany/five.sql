a6b6c9595446bd8292229bd8ef1252f96e811558
Masiu <ciszekmarcell@gmail.com>
2020-03-03 17:22:33 +0100

    longer tak query

--- basics2.0/manyToMany/five.sql
+++ basics2.0/manyToMany/five.sql
@@ -5,3 +5,14 @@ from series s
 group BY s.id
 order by avr_rating DESC
 limit 5;
+
+
+SELECT genre
+,
+       Round
+(Avg
+(rating), 2) AS avg_rating
+FROM   series
+       INNER JOIN reviews
+               ON series.id = reviews.series_id
+GROUP  BY genre;
