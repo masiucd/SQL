ff710b875bd6c8cf651f861610d1229e93fc5bed
Masiu <ciszekmarcell@gmail.com>
2020-03-06 15:46:34 +0100

    insta DB

--- sql.2.0/instaProj/mostLikes.sql
+++ sql.2.0/instaProj/mostLikes.sql
@@ -1,7 +1,25 @@
 -- who has most likes
-SELECT u.username , count(*) as mostLikes
+SELECT u.username, ph.image_url , count(*) as mostLikes
 from users u
   INNER JOIN likes s
   ON u.id = s.user_id
-GROUP BY u.username
-ORDER BY mostLikes DESC;
+  INNER JOIN photos ph
+  on u.id = ph.user_id
+GROUP BY ph.id
+ORDER BY mostLikes DESC
+limit 1;
+
+
+SELECT
+    username,
+    photos.id,
+    photos.image_url,
+    COUNT(*) AS total
+FROM photos
+INNER JOIN likes
+    ON likes.photo_id = photos.id
+INNER JOIN users
+    ON photos.user_id = users.id
+GROUP BY photos.id
+ORDER BY total DESC
+LIMIT 1;
