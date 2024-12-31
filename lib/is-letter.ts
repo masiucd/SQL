022fe94e728e065ae186c66624536286ec63604c
masiucd <ciszekmarcell@gmail.com>
2024-12-31 15:30:04 +0100

    Add functions to check for alphabetic and non-alphabetic characters

---
+++ lib/is-letter.ts
@@ -0,0 +1,25 @@
+/**
+ * Checks if the given character is an alphabetic character (A-Z or a-z).
+ *
+ * @param character - The character to check.
+ * @returns `true` if the character is alphabetic, `false` otherwise.
+ */
+export function isAlphabeticCharacter(character: string): boolean {
+  const alphabeticCharCodes = new Array(122 - 64)
+    .fill(0)
+    .map((_, i) => i + 64 + 1);
+  return alphabeticCharCodes.includes(character.charCodeAt(0));
+}
+
+/**
+ * Checks if the given character is a non-alphabetic character.
+ *
+ * This function returns `true` if the character is not an alphabetic character
+ * (i.e., not a letter from A-Z or a-z), and `false` otherwise.
+ *
+ * @param character - The character to check.
+ * @returns `true` if the character is non-alphabetic, `false` otherwise.
+ */
+export function isNonAlphabeticCharacter(character: string): boolean {
+  return !isAlphabeticCharacter(character);
+}
